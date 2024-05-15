f2d8df335fa50dfcacb4c6bbbcbc1b2447a93296
Danh Lê Hoàng <102210101@sv1.dut.udn.vn>
2024-05-15 19:51:16 +0700

    ini6

--- my_website/settings.py
+++ my_website/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee'
+    'employee',
+    'project',
+    'task',
+    
 ]
 
 MIDDLEWARE = [
