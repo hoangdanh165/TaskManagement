773b987a0b2754fa920e1fed4e7539532f8dfa09
Danh Lê Hoàng <102210101@sv1.dut.udn.vn>
2024-05-15 20:05:49 +0700

    ini8

--- my_website/settings.py
+++ my_website/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'employee',
     'project',
     'task',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'my_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
