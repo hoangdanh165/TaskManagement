293b94e75dbbc632a7e4257c5aa4415e54c215a5
Nguyen Dinh Anh Tien <hertyv72@gmail.com>
2024-05-18 03:03:49 +0700

    Alert message with progress bar

--- static/js/auto-dispossing-alert.js
+++ static/js/auto-dispossing-alert.js
@@ -1,5 +1,13 @@
-window.setTimeout(function() {
-    $(".alert").fadeTo(500, 0).slideUp(500, function(){
-        $(this).remove(); 
-    });
-}, 4000);
+$(document).ready(function() {
+    var $alert = $(".alert");
+    // Start the alert fade out after 4 seconds
+    window.setTimeout(function() {
+        $alert.fadeTo(500, 0).slideUp(500, function(){
+            $(this).remove(); 
+        });
+    }, 4000);
+});
+
+$(document).ready(function() {
+    $('.progress-bar').animate({width: "100%"}, 4000);
+});
