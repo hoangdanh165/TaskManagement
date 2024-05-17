aff4ee19a4700433d8d90aa89486aa91032f013a
Nguyen Dinh Anh Tien <hertyv72@gmail.com>
2024-05-17 15:38:58 +0700

    Add project 's due date and created date

--- project/models.py
+++ project/models.py
@@ -5,7 +5,9 @@ class Project(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     owner = models.ForeignKey('employee.User', on_delete=models.CASCADE, related_name='owned_projects')
-
+    due_date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.name
 
