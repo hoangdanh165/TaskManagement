f80b5d39f75f4fcd80f559c3163b9eaa0e5aaa1a
Danh Lê Hoàng <102210101@sv1.dut.udn.vn>
2024-05-19 21:42:15 +0700

    done changepassword

--- employee/views.py
+++ employee/views.py
@@ -1,9 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import RegistrationForm, ProfileUpdateForm, PasswordUpdateForm
+from .forms import RegistrationForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
 
 
 # Authentication
@@ -44,7 +44,6 @@ def sign_in(request):
     return render(request, 'employee/login-page.html')
 
 
-@login_required
 def sign_out(request):
     logout(request)
     return redirect('sign_in')
@@ -71,18 +70,32 @@ def view_user_profile(request):
 
 @login_required
 def change_password(request):
-    new_password = request.POST['new_password']
-    confirm_password = request.POST['confirm_password']
-    form = PasswordUpdateForm(request.POST, instance=request.user)
-    if form.is_valid():
-        form.save()
-        return redirect('view_user_profile')
-    else:
-        return render(request, 'employee/edit-user-profile.html')
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+
+        if not request.user.check_password(current_password):
+            messages.error(request, 'Current password is incorrect!')
+        elif new_password != confirm_password:
+            messages.error(request, 'Confirm password do not match!')
+        else:
+            request.user.set_password(new_password)
+            request.user.save()
+            # Keep user logged in
+            update_session_auth_hash(request, request.user)
+            messages.success(request, "Your password was successfully updated!")
+            return redirect('edit_user_profile')
+
+    return redirect('edit_user_profile')
 
 
 @login_required()
 def dashboard(request):
     title = "Dashboard"
+    user = request.user
+    projects = user.participated_projects.all()
+    for p in projects:
+        print(p.name)
     if request.user.is_authenticated:
-        return render(request, 'employee/dashboard.html', {'name': request.user.name, 'title': title})
+        return render(request, 'employee/dashboard.html', {'name': request.user.name, 'title': title, 'projects': projects})
