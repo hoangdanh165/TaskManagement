9da2b45c17ddcf2be36dcba73e6b85f3bed1fd75
Nguyen Dinh Anh Tien <hertyv72@gmail.com>
2024-05-19 22:30:28 +0700

    Filter tasks based on checked options

--- task/views.py
+++ task/views.py
@@ -10,11 +10,24 @@ from django.contrib import messages
 def tasks(request, project_id):
     project = Project.objects.get(id=project_id)
     
-    tasks = project.tasks.all()
+    task_range = request.GET.get('task_range', 'all')
+    if task_range == 'mine':
+        tasks = project.tasks.filter(assigned_to=request.user)
+    else:
+        tasks = project.tasks.all()
+    
+    task_status = request.GET.get('task_status', 'all')
+    if task_status == 'completed':
+        tasks = tasks.filter(completed=True)
+    elif task_status == 'in progress':
+        tasks = tasks.filter(completed=False)
+    
     context = { 
         'project': project, 
         'tasks': tasks,
         'actived_page': 'tasks',
+        'task_range': task_range,
+        'task_status': task_status,
     }
     
     return render(request, 'task/page-task.html', context)
