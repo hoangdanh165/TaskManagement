17acf78de260c7d27e2e50ecc5040b6f4af4416b
Nguyen Dinh Anh Tien <hertyv72@gmail.com>
2024-05-19 20:25:56 +0700

    Add project filter options

--- project/views.py
+++ project/views.py
@@ -31,9 +31,19 @@ def get_stored_form(request, form_class):
     return None
     
 def projects(request):
+    filter_by = request.GET.get('filter_by', 'all')
+    if filter_by == 'participated':
+        projects_list = request.user.participated_projects.all()
+    elif filter_by == 'mine':
+        projects_list = request.user.owned_projects.all()
+    else:
+        projects_list = Project.objects.all()
+    
+    # Continue filtering by search query
     search_query = request.GET.get('search', '')
-    projects_list = Project.objects.filter(name__icontains=search_query)
-    paginator = Paginator(projects_list, 6) # Show 10 projects per page.
+    projects_list = projects_list.filter(name__icontains=search_query)
+    
+    paginator = Paginator(projects_list, 6)
 
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
@@ -43,7 +53,8 @@ def projects(request):
     form = get_stored_form_or_create_one(request, ProjectForm)
     context = { 
         'page': page, 
-        'form': form, 
+        'form': form,
+        'filter_by': filter_by,
         'search_query': search_query,
         'view_option': view_option,
     }    
