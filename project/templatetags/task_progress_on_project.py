e6b7c1a2f17a272753f2d161fd0621ef3916c0b2
Nguyen Dinh Anh Tien <hertyv72@gmail.com>
2024-05-20 10:32:12 +0700

    Add user progress on navbar

--- project/templatetags/task_progress_on_project.py
+++ project/templatetags/task_progress_on_project.py
@@ -8,7 +8,7 @@ def task_progress_percentage(project, user):
     tasks_of_user_on_project = Task.objects.filter(belongs_to=project, assigned_to=user)
     total_tasks = tasks_of_user_on_project.count()
     completed_tasks = tasks_of_user_on_project.filter(completed=True).count()
-    return int(completed_tasks / total_tasks * 100) if total_tasks > 0 else 0
+    return int(completed_tasks / total_tasks * 100) if total_tasks > 0 else 100
 
 @register.filter
 def task_progress_text(project, user):
